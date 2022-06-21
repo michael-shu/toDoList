19f85b254a9108d461ce9204fb071a3ee33a10c3
Michael Shu <michael@modaramo.com>
2022-06-21 09:58:29 -0400

    Remove useless code, add more documentation

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use strict';
 
+/* Code to give functionality to the add button */
 const add = document.getElementById('add');
 add.addEventListener('click', function(){
     let test = document.querySelector('input').value;
 
+    /* Only if there's actually a value in input, should you start this whole process of creating a list element */
     if(test)
     {
         /* Create a node, add it to the ol using DOM*/
         let input = document.querySelector('input').value;
         let btnDone = document.createElement("button");
+
 
         
         btnDone.className = 'toggleCompleted';
@@ -21,6 +24,7 @@ add.addEventListener('click', function(){
         btnDone.addEventListener("click", () => {
             done();
     });
+
     function done() {
         //To designate the list item as checked for completion
         node.classList.toggle('checked');
@@ -34,7 +38,7 @@ add.addEventListener('click', function(){
     }
 });
 
-
+/* Clear automatically clears all elements that have been marked as completed by the user and moved to the completed list */
 const clear = document.getElementById('clear');
 clear.addEventListener('click',function(){
     let myList = document.getElementById('completed');
@@ -42,11 +46,14 @@ clear.addEventListener('click',function(){
 });
 
 
+/* Adds all elements to the unordered list that represents the completed tasks, and then removes all of them from the ordered list */
 const moveToCompleted = document.getElementById('completeButton');
 moveToCompleted.addEventListener('click',function(){
 
     let mover = document.getElementsByClassName('checked');
     let completed = document.getElementById('completed')
+
+    /* Adding loop */
     for(let i = 0; i<mover.length;i++)
     {
         let temp = mover[i].textContent;
@@ -56,24 +63,26 @@ moveToCompleted.addEventListener('click',function(){
         completed.append(node);
         
     }
+
+    /* Removing loop */
     for(let i = mover.length-1; i>=0; i--)
     {
         mover[i].remove();
     }
+
 });
 
+/* Simple function that removes the last bit of a string, which in this case is the checkmark so it doesn't appear in the unordered list of completed items */
 function removeCheckMark(str){
     let temp = str.slice(0,-1);
     return temp;
 }
 
+/* Adds the functionality of the dropdown menu, which allows the user to acess 3 buttons without taking up too much space */
 const toggle = document.getElementById('dropdownMenuToggle');
 const menu = document.getElementById('dropdownMenu');
 toggle.addEventListener('click',function(){
     menu.classList.toggle('show');
-
-    console.log(toggle.ariaExpanded);
-    console.log(typeof(toggle.ariaExpanded));
 
     if(toggle.ariaExpanded === 'true')
     {
